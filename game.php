0bd34f764388195f669bf04601f1ce56cc98a47b
Weston Renoud <wrenoud@gmail.com>
2014-08-17 02:15:35 -0300

    added buy economy method

--- game.php
+++ game.php
@@ -13,8 +13,12 @@ Class TritonGame {
         $this->id = $game_id;
     }
 
+    function order($type, $order){
+        return $this->client->gameRequest($type, $this->id, array('order'=>$order));
+    }
+
     function GetFullUniverse(){
-        return $this->client->gameRequest("order",$this->id,array('order' => "full_universe_report"));
+        return $this->order("order", "full_universe_report");
     }
     function GetIntel(){
         return $this->client->gameRequest("intel_data",$this->id);
@@ -38,4 +42,14 @@ Class TritonGame {
     function GetEventMessages($count, $offset = 0){
         return $this->GetMessages('game_event',$count, $offset);
     }
+
+    function BuyEconomy($star, $price){
+        return $this->order("batched_orders", "upgrade_economy,{$star},{$price}");
+    }
+    //function BuyIndustry($star, $price){
+    //    return $this->order("batched_orders", "upgrade_industry,{$star},{$price}");
+    //}
+    //function BuyScience($star, $price){
+    //    return $this->order("batched_orders", "upgrade_science,{$star},{$price}");
+    //}
 }
