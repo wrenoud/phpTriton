864f87b856a591b19243aad0787728b739d3917c
Brandon Dusseau <brandon.dusseau@gmail.com>
2019-12-04 19:19:47 -0500

    Fixes header case change in PHP 7.3+

--- util.php
+++ util.php
@@ -1,27 +1,26 @@
 <?php
 
-function curl_post($url, array $post = NULL, array $options = array()) 
-{ 
-    $defaults = array( 
-        CURLOPT_POST => 1, 
-        CURLOPT_HEADER => 0, 
-        CURLOPT_URL => $url, 
-        CURLOPT_FRESH_CONNECT => 1, 
-        CURLOPT_RETURNTRANSFER => 1, 
-        CURLOPT_FORBID_REUSE => 1, 
-        CURLOPT_TIMEOUT => 4, 
-        CURLOPT_POSTFIELDS => http_build_query($post) 
-    ); 
+function curl_post($url, array $post = NULL, array $options = array())
+{
+    $defaults = array(
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_URL => $url,
+        CURLOPT_FRESH_CONNECT => 1,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_FORBID_REUSE => 1,
+        CURLOPT_TIMEOUT => 4,
+        CURLOPT_POSTFIELDS => http_build_query($post)
+    );
 
-    $ch = curl_init(); 
-    curl_setopt_array($ch, ($options + $defaults)); 
-    if( ! $response = curl_exec($ch)) 
-    { 
-        trigger_error(curl_error($ch)); 
-    } 
-    //$request = curl_getinfo($ch);
+    $ch = curl_init();
+    curl_setopt_array($ch, ($options + $defaults));
+    if( ! $response = curl_exec($ch))
+    {
+        trigger_error(curl_error($ch));
+    }
     curl_close($ch);
-    return $response; 
+    return $response;
 }
 
 function http_parse($res){
@@ -34,8 +33,9 @@ function http_parse($res){
     foreach ($headers as $value) {
         $header = preg_split("/: /", $value);
         if(count($header) == 2){
-            switch($header[0]){
-                case "Set-Cookie":
+            $header_key = strtolower($header[0]);
+            switch($header_key){
+                case "set-cookie":
                     if(!array_key_exists("Set-Cookie", $parsed['headers'])){
                         $parsed['headers']['Set-Cookie'] = array();
                     }
@@ -45,11 +45,10 @@ function http_parse($res){
                     $parsed['headers']['Set-Cookie'][$cookie_name] = array_combine($values[1], $values[2]);
                     break;
                 default:
-                    $parsed['headers'][$header[0]] = $header[1];
+                    $parsed['headers'][$header_key] = $header[1];
             }
         }
     }
     $parsed['body'] = $matches[2];
     return $parsed;
 }
-
